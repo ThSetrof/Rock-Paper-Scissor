66b52ce06214170c489fd0796bac485a71441994
Tommy The Turtle <thsetrof@tuta.io>
2024-01-15 20:35:10 +0000

    Added function to play a round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice(){
-    let choice = Math.floor(Math.random) * 3;
+    let choice = Math.floor(Math.random() * 3);
 
     if(choice == 0) return 'paper';
 
@@ -9,3 +9,37 @@ function getComputerChoice(){
     if(choice == 2) return 'scissors';
 
 }
+
+
+function playRound(playerSelection, computerSelection){
+
+
+    if(!isPlayerSelectionValid(playerSelection)){
+        alert("Please enter 'rock', 'paper' or 'scissors'");
+        return;
+    }
+
+
+    if((playerSelection == 'paper' && computerSelection == 'rock') || 
+        playerSelection == 'rock' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'paper'){
+        return 'YOU WIN!!';
+
+    }else if(playerSelection == computerSelection){
+        return 'DRAW';
+
+    }else {
+        return 'YOU LOOSE';
+    }
+}
+
+function isPlayerSelectionValid(playerSelection){
+    return (playerSelection == 'paper' || playerSelection == 'scissors' || playerSelection == 'paper');
+}
+
+// let playerSelection = prompt("Enter player choice");
+let playerSelection = 'paper';
+let computerSelection = getComputerChoice();
+
+
+console.log(playerSelection, computerSelection, playRound(playerSelection, computerSelection));
